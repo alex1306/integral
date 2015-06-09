f4a9dc764e0538583cafafecb882fb6b7246c7e7
Semen <proffiblogger@gmail.com>
2015-06-09 10:33:51 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,38 @@
+# price for square sm
+price = '100.20'
+
+# system bluEvolution
+bluEvolution = '2.00'
+
+# system Design3D
+Design3D = '1.20'
+
+# system Streamline SL
+StreamlineSL = '1.30'
+
+# system Design 2D
+Design2D = '1.40'
+
+# podokonik
+podokonik = '0.03'
+
+# otliv
+otliv = '0.35'
+
+# otksos
+otkos = '0.25'
+
+1xlamination = '0.56'
+2xlamination = '0.80'
+
+
+
+
+
+
+
+
+
+
+
+
